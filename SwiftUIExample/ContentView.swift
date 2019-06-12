60e079f94cb7212490e1bf0da1d435949c902b83
이광용 <rhkdrmfh@gmail.com>
2019-06-11 23:17:27 -0400

    Initial Commit

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by 이광용 on 2019/06/11.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
